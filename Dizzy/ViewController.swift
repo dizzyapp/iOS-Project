c0ae547ba91bfd977c9b92d20a544aa57efe2ba1
ormenashe <menesh2@gmail.com>
2019-03-30 03:25:40 +0300

    Start Dizzy project and setup configurations

---
+++ Dizzy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Dizzy
+//
+//  Created by Or Menashe on 30/03/2019.
+//  Copyright © 2019 Dizzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.view.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
