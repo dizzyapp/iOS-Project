0c6f15de50dea88fc6350cc04631db266d36c984
Tal Ben Asuli <tal.ben-asuli@payboxapp.com>
2019-04-10 13:46:29 +0300

    fix(WebService-Firebase): added UUID for every post request.

--- Dizzy/Web Service/FirebaseWebService.swift
+++ Dizzy/Web Service/FirebaseWebService.swift
@@ -33,12 +33,18 @@ final class FirebaseWebService: WebServiceType {
                                                 completion: @escaping (Result<Response>) -> Void) {
         databaseReference.child(resource.path).observe(DataEventType.value) { snapshot in
             do {
-                guard let json = snapshot.value as? [String: Any] else {
+            
+                guard let jsonsArray = snapshot.value as? [String: Any] else {
                     completion(Result<Response>.failure(WebServiceError.noData))
                     return
                 }
                 
-                let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+                var jsonToParse = [[String: Any]]()
+                for (_,value) in jsonsArray {
+                    jsonToParse.append(value as! [String : Any])
+                }
+                
+                let data = try JSONSerialization.data(withJSONObject: jsonToParse, options: .prettyPrinted)
                 let parsedData = try resource.parse(data: data)
                 completion(Result<Response>.success(parsedData))
             } catch let jsonError {
@@ -50,14 +56,14 @@ final class FirebaseWebService: WebServiceType {
     private func sendPostRequest<Response, Body>(resource: Resource<Response, Body>,
                                                  completion: @escaping (Result<Response>) -> Void) {
         if let json = resource.makeJson() {
-            databaseReference.child(resource.path).setValue(json) { (error, _) in
+            databaseReference.child("\(resource.path)/\(UUID())").setValue(json) { (error, _) in
                 if error != nil {
                     completion(Result<Response>.failure(error))
                 }
             }
         }
     }
-    
+
     func shouldHandle<Response, Body>(_ resource: Resource<Response, Body>) -> Bool where Response : Decodable, Response : Encodable, Body : Encodable {
         return true
     }
