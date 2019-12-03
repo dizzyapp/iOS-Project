1299caa2d99e68bbd8b1e87583583827498b0e8b
Tal Ben Asuli <tal.ben-asuli@payboxapp.com>
2019-12-03 22:07:15 +0200

    feature(admin_settings_remake): finished with reservationCell UI.

--- Dizzy/Screens/AdminSettings/AdminPlaceAnalytics/ReservationCell.swift
+++ Dizzy/Screens/AdminSettings/AdminPlaceAnalytics/ReservationCell.swift
@@ -7,23 +7,71 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class ReservationCell: UITableViewCell {
     
-    let nameLabel = UILabel()
-    let numberOfPeopleLabel = UILabel()
-    let iconImageView = PlaceImageView()
+    private let iconImageHeight = CGFloat(40)
+    
+    private let nameLabel = UILabel()
+    private let numberOfPeopleLabel = UILabel()
+    private let iconImageView = PlaceImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .green
+        setupViews()
+        addSubviews()
+        layoutViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure(with data: ReservationData) {
+    private func setupViews() {
+        setupNameLabel()
+        setupNumberOfPeopleLabel()
+    }
+    
+    private func setupNameLabel() {
+        nameLabel.textColor = .primeryPurple
+        nameLabel.numberOfLines = 1
+        nameLabel.font = Fonts.h6(weight: .bold)
+    }
+    
+    private func setupNumberOfPeopleLabel() {
+        numberOfPeopleLabel.numberOfLines = 1
+        numberOfPeopleLabel.font = Fonts.h6(weight: .bold)
+    }
+    
+    private func addSubviews() {
+        contentView.addSubviews([nameLabel, numberOfPeopleLabel, iconImageView])
+    }
+    
+    private func layoutViews() {
+        iconImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(Metrics.padding)
+            make.leading.equalToSuperview().offset(Metrics.oneAndHalfPadding)
+            make.height.width.equalTo(iconImageHeight)
+        }
         
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(iconImageView.snp.trailing).offset(Metrics.padding)
+            make.centerY.equalTo(iconImageView)
+        }
+        
+        numberOfPeopleLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(Metrics.triplePadding)
+            make.centerY.equalTo(nameLabel)
+        }
+    }
+    
+    func configure(with data: ReservationData) {
+        numberOfPeopleLabel.text = "\(data.numberOfPeople ?? 0)"
+        nameLabel.text = data.clientName
+        
+        if let url = URL(string: data.iconImageURLString ?? "") {
+            iconImageView.setImage(from: url)
+        }
     }
 }
